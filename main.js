04ad3272e40e23f71483e0a7655040aa7f8e0aa1
Anggito Anju Hartawan Manalu <manaluanggito@gmail.com>
2021-09-11 08:48:38 +0700

    NAVBAR JS FINISH

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 // Navbar Animation
 
-console.log(document.body.scrollTop);
+var nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function (event) {
+    event.preventDefault();
+
+    if (window.scrollY >= 508) {
+        nav.style.backgroundColor = '#BBC8DD';
+        nav.style.color = '#030824';
+        nav.style.transitionDuration = '0.4s';
+    } else {
+        nav.style.backgroundColor = 'transparent';
+    }
+});
